faaa3b25c0d30f05ce755acd2c818de41cf1dfae
Yusuf <yscnpkr@gmail.com>
2025-01-27 19:57:08 +0300

    maintaining

--- index.js
+++ index.js
@@ -1,167 +1,160 @@
-const inquirer = require('inquirer');
-const low = require('lowdb');
+import inquirer from 'inquirer';
+import low from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync.js';
 
-const FileSync = require('lowdb/adapters/FileSync');
-
-const ToDoDataAdapter = new FileSync('./databases/ToDo.json');
-const tddb = low(ToDoDataAdapter);
-
+const ToDoDataAdapter = new FileSync('database.json');
+const db = low(ToDoDataAdapter);
 const prompt = inquirer.createPromptModule();
 
-function createTDID() {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    for (var i=0;i<3;i++) {
+function generateId() {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    for (let i=0;i<8;i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
-   };
+   }
    return result;
-};
+}
 
-prompt([{
-    name: 'process',
-    message: 'Welcome back sir! How can i help you?',
-    type: 'list',
-    choices: ["Show me the ToDo List", "Add a New item to the ToDo List", "Remove an item from the ToDo List", "Create a new ToDo category", "Delete a ToDo Category"],
-    loop: true
-}]).then(async(answer) => {
-    if (answer.process == 'Show me the ToDo List') {
-        const cats = [];
-        tddb.get('category').value().forEach(async(data) => cats.push(data.catname));
-        cats.push('No category');
+while (true) {
+    console.clear();
+    await prompt([{
+        name: 'thing',
+        message: 'Welcome back sir! How may i help you?',
+        type: 'list',
+        choices: [
+            {
+                name: "Show me the todo list",
+                short: 'List',
+                value: 1
+            },
+            {
+                name: "Add a New item to the todo list",
+                short: 'Add',
+                value: 2
+            },
+            {
+                name: "Remove an item from the todo list",
+                short: 'Remove',
+                value: 3
+            },
+            {
+                name: "Create a new category",
+                short: 'Create',
+                value: 4
+            },
+            {
+                name: "Delete a category",
+                short: 'Delete',
+                value: 5
+            },
+            {
+                name: 'Exit',
+                value: 6
+            }
+        ]
+    }]).then(({ thing }) => {
+        db.read();
+        let categories = Array.from(db.get('categories').value().map(a => a.name));
+        categories.push({ name: 'No category', value: 'nocategory' });
 
-        prompt([{
-            name: 'cat',
-            message: 'Select a To-Do List category..',
-            type: 'list',
-            choices: cats
-        }]).then(async(a) => {
-            var category = 'nocategory';
-            if (a.cat !== 'No category') category = 'custom';
+        switch (thing) {
+            case 1: // show the list
+                prompt([{
+                    name: 'category',
+                    message: 'Select a Todo category..',
+                    type: 'list',
+                    choices: categories
+                }]).then(({ category }) => {
+                    const todos = db.get('todo').value().filter(a => a.category === category).map(a => a.todo);
+                    if (todos.length === 0) return console.log('No item added to the ToDo List was found.');
 
-            const todos = [];
-            if (category == 'nocategory') {
-                tddb.read();
-                tddb.get('ToDo').value().forEach(async(data) => {
-                    if (data.category == 'nocategory') {
-                        todos.push(data.todo);
-                    };
+                    return console.log(todos.join('\n'));
                 });
-            };
+                break;
+            case 2: // add new item
+                prompt([{
+                    name: 'category',
+                    message: 'Please select a ToDo category',
+                    type: 'list',
+                    choices: categories
+                }]).then(({ category }) => {
+                    prompt([{
+                        name: 'todo',
+                        message: 'Type the item to be added to the ToDo List.',
+                        type: 'input',
+                    }]).then(async({ todo }) => {
+                        await db.get('todo').push({ date: Date.now(), todo: `・${todo}`, id: generateId(), category: category }).write();
+                        console.log('Success! New Todo list;')
 
-            tddb.read();
-            tddb.get('ToDo').value().forEach(async(data) => {
-                if (data.category == a.cat) {
-                    todos.push(data.todo);
-                };
-            });
-            
-            if (todos.length == 0) return console.log('No item added to the ToDo List was found.');
-
-            return console.log(todos.join('\n'));
-        });
-    } else if (answer.process == 'Add a New item to the ToDo List') {
-        var cats = [];
-        tddb.read();
-        tddb.get('category').value().forEach(async(data) => cats.push(data.catname));
-        cats.push('No category');
-
-        prompt([{
-            name: 'selectcat',
-            message: 'Please select a ToDo category',
-            type: 'list',
-            choices: cats
-        }]).then(async(a) => {
-            var category = a.selectcat;
-            if (a.selectcat == 'No category') { category == 'nocategory' };
-
-            prompt([{
-                name: 'newitem',
-                message: 'Type the item to be added to the ToDo List.',
-                type: 'input',
-            }]).then(async(answer) => {
-                tddb.read();
-                var toDoList = [];
-                tddb.get('ToDo').value().forEach(async(data) => toDoList.push(data.todo))
-                tddb.get('ToDo').push({ date: Date.now(), todo: '・'+answer.newitem, id: createTDID(), category: category }).write();
-    
-                console.log('The new item has been successfully added to the ToDo List. New ToDo List;')
-    
-                toDoList = [];
-    
-                tddb.read();
-                tddb.get('ToDo').value().forEach(async(data) => {
-                    if (data.category == category) toDoList.push(data.todo);
+                        db.read();
+                        const todos = db.get('todo').value().filter(a => a.category === category).map(a => a.todo);
+                        console.log(todos.join('\n'));
+                    });
                 });
-                toDoList.forEach(async(data) => console.log(data));
-            });
-        });
-    } else if (answer.process == 'Remove an item from the ToDo List') {
-        tddb.read();
-        var toDoList = [];
-        tddb.get('ToDo').value().forEach(async(data) => toDoList.push(data.todo));
-        if (toDoList.length == 0) {
-            return console.log('No item added to the ToDo List was found.');
-        } else {
-            prompt([{
-                name: 'delete',
-                message: 'Select the item to be deleted.',
-                type: 'list',
-                choices: toDoList
-            }]).then(async(answer) => {
-                tddb.read();
-                const todo = tddb.get('ToDo').find({ todo: answer.delete }).value();
-                await tddb.get('ToDo').remove({ id: todo.id }).write();
+                break;
+            case 3: // remove item
+                const todos = db.get('todo').value().map(a => { return { name: a.todo, value: a.id } });
+                if (todos.length === 0) return console.log('no item found');
 
-                console.log('The item has been successfully deleted.');
-            });
+                prompt({
+                    name: 'id',
+                    message: 'Select the item to delete',
+                    type: 'list',
+                    choices: todos
+                }).then(async ({ id }) => {
+                    await db.get('todo').remove({ id }).write();
+                    console.log('item deleted successfully.');
+                });
+                break;
+            case 4: // create category
+                prompt([{
+                    name: 'category',
+                    message: 'Write a category name',
+                    type: 'input'
+                }]).then(async ({ category }) => {
+                    if (category === '') return console.log('Please write a valid category name');
+
+                    let categories = Array.from(db.get('categories').value());
+                    if (categories.includes(category)) return console.log(`there is already a category named ${category}`);
+
+                    await db.get('categories').push({ date: Date.now(), id: generateId(), name: category }).write();
+                    db.read();
+                    categories = db.get('categories').value().map(a => a.name);
+
+                    return console.log('\nCategory successfully created, new category list;\n'+categories.join('\n'));
+                });
+                break;
+            case 5: // delete category
+                categories = db.get('categories').value().map(a => { return { name: a.name, value: { name: a.name, id: a.id } } });
+
+                prompt([{
+                    name: 'category',
+                    message: 'select the category to be deleted',
+                    type: 'list',
+                    choices: categories
+                }]).then(async ({ category }) => {
+                    const todoCategories = db.get('todo').value().map(a => a.category);
+                    if (!todoCategories.includes(category.name)) {
+                        db.get('categories').remove({ id: category.id }).write();
+                        return console.log('Category deleted successfully.');
+                    }
+
+                    prompt({
+                        name: 'confirm',
+                        message: 'This category contains some todos. Sure you want to delete it?',
+                        type: 'confirm'
+                    }).then(async({ confirm }) => {
+                        if (!confirm) return console.log(`check the todo list for ${category.name}`);
+                        db.get('categories').remove({ id: category.id }).write();
+                        return console.log('Category deleted successfully.');
+                    });
+                });
+                break;
+            case 6:
+                console.log('Bye!');
+                process.exit(0);
+                break;
         }
-    } else if (answer.process == 'Create a new ToDo category') {
-        prompt([{
-            name: 'cat',
-            message: 'Write a category name',
-            type: 'input'
-        }]).then(async(a) => {
-            if (a.cat == '') return console.log('Please write a valid category name');
-
-            var cats = [];
-            tddb.read();
-            tddb.get('category').value().forEach(async(data) => cats.push(data));
-
-            if (cats.includes(a.cat)) return console.log('Already created such a category');
-
-            tddb.read();
-            tddb.get('category').push({ date: Date.now(), catname: a.cat }).write();
-
-            cats = [];
-            tddb.read();
-            tddb.get('category').value().forEach(async(data) => cats.push(data.catname));
-
-            return console.log('\nCategory succesfully created, new category list;\n'+cats.join('\n'));
-        });
-    } else if (answer.process == 'Delete a ToDo Category') {
-        var cats = [];
-        tddb.read();
-        tddb.get('category').value().forEach(async(data) => cats.push(data.catname));
-
-        prompt([{
-            name: 'delcat',
-            message: 'Type the name of the category to be deleted',
-            type: 'list',
-            choices: cats
-        }]).then(async(a) => {
-            var todos = [];
-            tddb.read();
-            tddb.get('ToDo').value().forEach(async(data) => todos.push(data.category));
-
-            if (todos.includes(a.delcat)) return console.log('You cant remove the category before you remove the todo..')
-
-            cats = [];
-            tddb.read();
-            tddb.get('category').remove({ catname: `${a.delcat}` }).write();
-            tddb.get('category').value().forEach(async(data) => cats.push(data.catname));
-
-            if (cats.length == 0) return console.log('\nCategory succesfully deleted!');
-            return console.log('\nCategory succesfully deleted, new category list;\n'+cats.join('\n'));
-        });
-    };
-});
+    });
+    await new Promise(resolve => setTimeout(resolve, 3000));
+}
